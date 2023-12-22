133e3410ee24a2cfb57919bc2ab3c45eeea0755f
Mark <mark.grabovski@gmail.com>
2023-12-22 10:14:13 +0200

    deleted Book class

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,3 @@
-INSERT INTO books (title, author, genre, publishing_date, copies_sold)
-VALUES ('Clean Code', 'Robert Cecil Martin', 'Computer & Technology', DATE '2008-08-01', 1000);
-INSERT INTO books (title, author, genre, publishing_date, copies_sold)
-VALUES ('Dune', 'Frank Herbert', 'Sci-Fi', DATE '2008-08-01', 25000);
-INSERT INTO books (title, author, genre, publishing_date, copies_sold)
-VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', 'Tragedy', DATE '1925-04-25', 28000);
-INSERT INTO books (title, author, genre, publishing_date, copies_sold)
-VALUES ('20,000 Leagues Under the Sea', 'Jules Verne', 'Adventure', DATE '1869-03-11', 18550);
-INSERT INTO books (title, author, genre, publishing_date, copies_sold)
-VALUES ('Guns, Germs, and Steel', 'Jared Diamond', 'Nonfiction', DATE '1997-01-01', 13550);
-
 INSERT INTO filters (id, name)
 VALUES (100000000, 'My Filter 1');
 INSERT INTO filters (id, name)
